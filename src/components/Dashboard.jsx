4d6af861a607412e0176872b00376547b00ba5b0
rohitswami16 <rohitswami1612@gmail.com>
2025-02-16 22:47:59 +0530

    Initial commit for OTP authentication project

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,307 @@
+import { Link } from "react-router-dom";
+import { FaHome, FaCog } from "react-icons/fa";
+import styles from "./Dashboard.module.css";
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    sessionStorage.removeItem("authSession");
+    navigate("/");
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <h1>Analytics Dashboard</h1>
+      <h2 onClick={handleLogout} style={{ cursor: "pointer", color: "white" }}>
+        Logout
+      </h2>
+    </nav>
+  );
+};
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <Link to="/" className={styles.sidebarIcon}>
+        <FaHome />
+      </Link>
+      <Link to="/settings" className={styles.sidebarIcon}>
+        <FaCog />
+      </Link>
+    </div>
+  );
+};
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    &copy; 2025, Greendzine Technologies Pvt. Ltd. All Rights Reserved.
+  </footer>
+);
+
+const Dashboard = () => {
+  const inventoryChartRef = useRef(null);
+  const ordersChartRef = useRef(null);
+  const batteryChartRef = useRef(null);
+  const marginChartRef = useRef(null);
+
+  useEffect(() => {
+    // inventory chart
+    const inventoryChart = new Chart(inventoryChartRef.current, {
+      type: "line",
+      data: {
+        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        datasets: [
+          {
+            label: "Inventory",
+            data: [70, 80, 75, 90, 85, 95, 93],
+            borderColor: "#ffa500",
+            tension: 0.4,
+            fill: true,
+            borderWidth: 2,
+            pointRadius: 5,
+            pointBackgroundColor: "#E79500",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            grid: { display: false },
+            ticks: { color: "#fff" },
+          },
+          y: {
+            beginAtZero: true,
+            grid: { color: "rgba(255, 255, 255, 0.1)" },
+            ticks: { color: "#fff" },
+          },
+        },
+        plugins: {
+          legend: { display: false },
+        },
+      },
+    });
+
+    // order chart
+    const ordersChart = new Chart(ordersChartRef.current, {
+      type: "line",
+      data: {
+        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        datasets: [
+          {
+            label: "Orders",
+            data: [50, 60, 55, 70, 65, 75, 65],
+            borderColor: "#ff0055",
+            tension: 0.4,
+            fill: true,
+            borderWidth: 2,
+            pointRadius: 5,
+            pointBackgroundColor: "#D40A50",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            grid: { display: false },
+            ticks: { color: "#fff" },
+          },
+          y: {
+            beginAtZero: true,
+            grid: { color: "rgba(255, 255, 255, 0.1)" },
+            ticks: { color: "#fff" },
+          },
+        },
+        plugins: {
+          legend: { display: false },
+        },
+      },
+    });
+
+    // battery chart
+    const batteryChart = new Chart(batteryChartRef.current, {
+      type: "doughnut",
+      data: {
+        labels: ["Remaining", "Consumed"],
+        datasets: [
+          {
+            data: [65, 35],
+            backgroundColor: ["#007bff", "#ffcc00"],
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: "bottom",
+            labels: {
+              font: {
+                size: 14,
+              },
+              color: "#ffffff",
+            },
+          },
+        },
+      },
+      plugins: [
+        {
+          id: "centerText",
+          beforeDraw: function (chart) {
+            const { width } = chart;
+            const { height } = chart;
+            const ctx = chart.ctx;
+            ctx.restore();
+
+            const fontSize = (height / 8).toFixed(2);
+            ctx.font = `${fontSize}px Arial`;
+            ctx.textBaseline = "middle";
+            ctx.textAlign = "center";
+
+            const centerX = width / 2;
+            const centerY = height / 2;
+
+            ctx.fillStyle = "#ffffff";
+            ctx.fillText("65%", centerX, centerY);
+            ctx.save();
+          },
+        },
+      ],
+    });
+
+    // margin chart
+    const marginChart = new Chart(marginChartRef.current, {
+      type: "line",
+      data: {
+        labels: [
+          "12th Oct",
+          "13th Oct",
+          "14th Oct",
+          "15th Oct",
+          "16th Oct",
+          "17th Oct",
+        ],
+        datasets: [
+          {
+            label: "Margin %",
+            data: [40, 50, 80, 60, 90, 75],
+            borderColor: "#aaff00",
+            backgroundColor: "rgba(170, 255, 0, 0.2)",
+            tension: 0.4,
+            fill: true,
+            borderWidth: 2,
+            pointRadius: 5,
+            pointBackgroundColor: "#aaff00",
+          },
+        ],
+      },
+
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            grid: { display: false },
+            ticks: { color: "#fff" },
+          },
+          y: {
+            beginAtZero: true,
+            grid: { color: "rgba(255, 255, 255, 0.1)" },
+            ticks: { color: "#fff" },
+          },
+        },
+        plugins: {
+          legend: { display: false },
+        },
+      },
+    });
+
+    return () => {
+      inventoryChart.destroy();
+      ordersChart.destroy();
+      batteryChart.destroy();
+      marginChart.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+      <div className="dashboard-container">
+        {/* Inventory Card */}
+        <div className="main">
+          {/* Left Side - Inventory & Orders */}
+
+          <div className="smallcard">
+            <div className="card-header">
+              <span>Inventory</span>
+              <span>7 days ▼</span>
+            </div>
+            <h2 className="card-value">93%</h2>
+            <div className="chart-box">
+              <canvas ref={inventoryChartRef}></canvas>
+            </div>
+          </div>
+          <br />
+          <div className="smallcard">
+            <div className="card-header">
+              <span>Orders</span>
+              <span>7 days ▼</span>
+            </div>
+            <h2 className="card-value">65%</h2>
+            <div className="chart-box">
+              <canvas ref={ordersChartRef}></canvas>
+            </div>
+          </div>
+        </div>
+
+        {/* Battery card*/}
+        <div className="main">
+          <div className="right-side">
+            <div className="card-header1">
+              <span>Battery</span>
+              <span>...</span>
+            </div>
+            <div className="battery-card">
+              <div className="battery-chart">
+                <canvas ref={batteryChartRef}></canvas>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br />
+        <br />
+        {/* Margin Card */}
+        <div className="main">
+          <div className="card-large">
+            <div className="card-header3">
+              <span>Margin %</span>
+              <span>7 days ▼</span>
+            </div>
+            <div className="chart-box3">
+              <canvas ref={marginChartRef}></canvas>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Login.jsx
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import emailjs from "@emailjs/browser";
+import "../styles.css";
+import styles from "./Dashboard.module.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [otpSent, setOtpSent] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [generatedOtp, setGeneratedOtp] = useState("");
+  const [timer, setTimer] = useState(30);
+  const navigate = useNavigate();
+
+  // navbar
+  const Navbar = () => (
+    <nav className={styles.navbar}>
+      <h1>Analytics Dashboard</h1>
+    </nav>
+  );
+
+  // footer
+  const Footer = () => (
+    <footer className={styles.footer}>
+      &copy; 2025, Greendzine Technologies Pvt. Ltd. All Rights Reserved.
+    </footer>
+  );
+
+  useEffect(() => {
+    if (otpSent && timer > 0) {
+      const interval = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [otpSent, timer]);
+
+  // send Otp
+  const sendOtp = async () => {
+    if (!email) {
+      alert("Please enter an email");
+      return;
+    }
+
+    // 6 Digit Otp
+    const newOtp = Math.floor(100000 + Math.random() * 900000).toString();
+    setGeneratedOtp(newOtp);
+    setOtpSent(true);
+    setTimer(30);
+
+    const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+
+    if (!serviceID || !templateID || !publicKey) {
+      // alert("EmailJS credentials are missing. Check your .env file.");
+      return;
+    }
+
+    emailjs
+      .send(serviceID, templateID, { to_email: email, otp: newOtp }, publicKey)
+      .then(() => {
+        alert(`OTP has been sent to ${email}`);
+      })
+      .catch((error) => {
+        // console.error("EmailJS Error:", error);
+        alert("Failed to send OTP. Try again.");
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (otp === generatedOtp) {
+      navigate("/dashboard");
+    } else {
+      alert("Invalid OTP");
+    }
+  };
+
+  return (
+    <div className="mainbody">
+      <Navbar />
+      <div className="login-container">
+        <div className="outercard">
+          <div className="sideby">
+            {!otpSent ? (
+              <div className="email-container">
+                <h2 style={{ color: "white" }}>Sign In</h2>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="E-Mail"
+                  required
+                />
+                <br />
+                <button onClick={sendOtp}>Send OTP</button>
+              </div>
+            ) : (
+              <div className="otp-container">
+                <h2
+                  style={{
+                    color: "white",
+                    margin: "10px auto",
+                    textAlign: "center",
+                  }}
+                >
+                  Enter OTP sent to Email
+                </h2>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
+                    placeholder="OTP"
+                    required
+                  />
+                  <br />
+                  {timer > 0 ? (
+                    <p className="timer-text">Resend OTP in {timer} sec</p>
+                  ) : (
+                    <button
+                      type="button"
+                      className="resendbtn"
+                      onClick={sendOtp}
+                      style={{
+                        display: "block",
+                        margin: "10px auto",
+                        fontSize: "12px",
+                        padding: "5px 8px",
+                      }}
+                    >
+                      Resend OTP
+                    </button>
+                  )}
+                  <br />
+                  <button
+                    type="submit"
+                    className="validate-btn"
+                    style={{ display: "block", margin: "10px auto" }}
+                  >
+                    Validate
+                  </button>
+                </form>
+              </div>
+            )}
+          </div>
+          <div className="sideby1">
+            <h3>Web application with Analytics dashboard</h3>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
